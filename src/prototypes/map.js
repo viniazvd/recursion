875a933e8d8cae10af57e3eb071a7b2036489a00
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-18 00:12:24 -0200

    done -v0.01

---
+++ src/prototypes/map.js
@@ -0,0 +1,10 @@
+const map = (fn, array) => {
+  if (typeof fn !== 'function') throw new TypeError()
+  if (array.length < 0) throw new TypeError()
+
+  return !array.length
+    ? []
+    : [fn(array[0])].concat(map(fn, array.slice(1)))
+}
+
+module.exports = { map }
