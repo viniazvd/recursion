a8695aae338ce2dec31e5d9c758e65eb94d4ef6d
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-17 23:07:11 -0200

    add filter tests

---
+++ src/test/filter.test.js
@@ -0,0 +1,42 @@
+const { len } = require('../utils')
+const { filter, countRepeated } = require('../prototypes/filter')
+
+describe('len', () => {
+  it('empty array', () => {
+    expect(len([])).toEqual(0)
+  })
+
+  it('n elements', () => {
+    expect(len([1, 2, 3])).toEqual(3)
+  })
+})
+
+describe('filter', () => {
+  const greaterThan2 = x => x > 2
+
+  it('empty array', () => {
+    expect(filter(greaterThan2, [])).toEqual([])
+  })
+
+  it('filter n elements', () => {
+    expect(filter(greaterThan2, [1, 2, 3, 4, 5])).toEqual([3, 4, 5])
+  })
+
+  it('return empty', () => {
+    expect(filter(greaterThan2, [1, 2])).toEqual([])
+  })
+})
+
+describe('countRepeated', () => {
+  it('empty array', () => {
+    expect(countRepeated(10, [])).toEqual(0)
+  })
+
+  it('some repeated', () => {
+    expect(countRepeated(10, [1, 2, 10, 1, 10, 10])).toEqual(3)
+  })
+
+  it('none repeated', () => {
+    expect(countRepeated(10, [1, 2, 3, 4])).toEqual(0)
+  })
+})
