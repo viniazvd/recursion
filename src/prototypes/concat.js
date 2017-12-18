e554f129adbd1aab7c53cd6d24704c35f044c971
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-18 03:36:14 -0200

    done -v0.0.2

--- src/prototypes/concat.js
+++ src/prototypes/concat.js
@@ -1,11 +1,1 @@
-const concat = (array1, array2) => {
-  if (!array2.length) {
-    return array1
-  } else {
-    array1.push(array2[0])
-    array2.shift()
-    return concat(array1, array2)
-  }
-}
-
-module.exports = concat
+module.exports = (array1, array2) => [...array1, ...array2]
--- src/test/concat.test.js
+++ src/test/concat.test.js
@@ -9,6 +9,10 @@ describe('concat', () => {
     expect(concat([1, 2, 3], [])).toEqual([1, 2, 3])
   })
 
+  it('arrays empty', () => {
+    expect(concat([], [])).toEqual([])
+  })
+
   it('when is valid', () => {
     expect(concat([1, 2, 3], [4, 5, 6])).toEqual([1, 2, 3, 4, 5, 6])
   })
