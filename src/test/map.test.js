c733a65211855939788c4a8892821bff7c84d927
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-18 01:09:27 -0200

    separated tests

--- src/test/map.test.js
+++ src/test/map.test.js
@@ -5,6 +5,13 @@ describe('map', () => {
     const multiply2 = x => x * 2
 
     expect(map(multiply2, [])).toEqual([])
+  })
+})
+
+describe('map', () => {
+  it('empty array', () => {
+    const multiply2 = x => x * 2
+
     expect(map(multiply2, [1, 2, 3])).toEqual([2, 4, 6])
   })
 })
