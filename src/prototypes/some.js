065398707b3bc31b7bca24a1f2feed757ffefcee
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-18 02:43:12 -0200

    done -v0.0.2

--- src/prototypes/some.js
+++ src/prototypes/some.js
@@ -1,9 +1,13 @@
 const some = (fn, array) => {
   if (typeof fn !== 'function') throw new TypeError()
 
+  // return !array.length
+  // ? []
+  // : (fn(array[0]) ? [true] : [false]).concat(some(fn, array.slice(1))).includes(true)
+
   return !array.length
-  ? []
-  : (fn(array[0]) ? [true] : [false]).concat(some(fn, array.slice(1))).includes(true)
+  ? false
+  : (fn(array[0]) ? true : some(fn, array.slice(1)))
 }
 
 module.exports = { some }
