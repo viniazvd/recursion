ed0fe338706713c9417598ab2732d8dd563a5304
lubien <lubien1996@gmail.com>
2017-12-18 16:48:32 -0300

    Add `fill` failing test

--- src/test/fill.test.js
+++ src/test/fill.test.js
@@ -8,4 +8,8 @@ describe('fill', () => {
   it('when is valid', () => {
     expect(fill([1, 2, 3], 4)).toEqual([4, 4, 4])
   })
+
+  it('when the value is also inside', () => {
+    expect(fill([1, 1, 1], 1)).toEqual([1, 1, 1])
+  });
 })
