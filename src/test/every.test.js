b1ca2e0a79a2dd64ed5e77d1863a7a53c1fb15a3
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-18 02:15:11 -0200

    change describe test

--- src/test/every.test.js
+++ src/test/every.test.js
@@ -1,22 +1,18 @@
-const { len } = require('../utils')
+// const { len } = require('../utils')
 const { every } = require('../prototypes/every')
 
 describe('every', () => {
-  it('empty array', () => {
-    expect(len([])).toEqual(0)
-  })
-})
+  // it('empty array', () => {
+  //   expect(len([])).toEqual(0)
+  // })
 
-describe('every', () => {
-  it('all data are less than 6', () => {
+  it('when is valid', () => {
     const lessThan = x => x < 6
 
     expect(every(lessThan, [1, 2, 3, 4, 5])).toEqual(true)
   })
-})
 
-describe('every', () => {
-  it('all data are greater than 2', () => {
+  it('when is invalid', () => {
     const lessThan = x => x > 2
 
     expect(every(lessThan, [1, 2, 3, 4, 5])).toEqual(false)
