bbb917edf537e83bce2ff0a2891a18a15bea3aed
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-18 23:05:07 -0200

    refactoring ternary inline

--- src/prototypes/shift.js
+++ src/prototypes/shift.js
@@ -3,9 +3,7 @@ const empty = require('../utils/empty')
 const shift = array => {
   return empty(array)
     ? []
-    : array.length > 1
-      ? [array[1]].concat(shift(array.slice(1)))
-      : []
+    : array.length > 1 ? [array[1]].concat(shift(array.slice(1))) : []
 }
 
 module.exports = shift
