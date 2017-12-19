9556c666c9318f8ba59d7b764dbe58cb3e51a425
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-19 14:07:36 -0200

    adding test to value not found

--- src/test/find.test.js
+++ src/test/find.test.js
@@ -1,13 +1,18 @@
 const find = require('../prototypes/find')
 
 describe('find', () => {
-  const multiply2 = x => x === 2
+  const find2 = x => x === 2
+  const find7 = x => x === 7
 
   it('empty array', () => {
-    expect(find(multiply2, [])).toEqual([])
+    expect(find(find2, [])).toEqual([])
   })
 
   it('multiply data by 2', () => {
-    expect(find(multiply2, [1, 2, 3, 4, 5, 6])).toEqual(2)
+    expect(find(find2, [1, 2, 3, 4, 5, 6])).toEqual(2)
+  })
+
+  it('no founded value', () => {
+    expect(find(find7, [1, 2, 3, 4, 5, 6])).toEqual([])
   })
 })
