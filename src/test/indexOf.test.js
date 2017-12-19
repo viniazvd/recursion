b25d88b6d04e8317a5c7abbce0dc580a0414c10e
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-19 15:23:46 -0200

    created indexOf test functions

---
+++ src/test/indexOf.test.js
@@ -0,0 +1,15 @@
+const indexOf = require('../prototypes/indexOf')
+
+describe('indexOf', () => {
+  it('empty array', () => {
+    expect(indexOf(3, [])).toEqual(-1)
+  })
+
+  it('find 2 in array', () => {
+    expect(indexOf(2, [1, 2, 3, 4, 5, 6])).toEqual(2)
+  })
+
+  it('no founded value', () => {
+    expect(indexOf(7, [1, 2, 3, 4, 5, 6])).toEqual(-1)
+  })
+})
