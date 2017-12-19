682db1b4de14a5f3c628bfc2f1a52d7c51d0f077
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-19 17:40:32 -0200

    refactoring plus isEqual function

--- src/prototypes/indexOf.js
+++ src/prototypes/indexOf.js
@@ -1,9 +1,10 @@
 const find = require('./find')
+const isEqual = x => y => x === y
 
 const indexOf = (value, array) => {
-  return !find(x => x === value, array)
+  return !find(isEqual(value), array)
   ? -1
-  : array.findIndex(x => x === value)
+  : array.findIndex(isEqual(value))
 }
 
 module.exports = indexOf
