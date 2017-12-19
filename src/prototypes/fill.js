be5a0255aada85452d8858000982af56be9b8444
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-19 15:11:51 -0200

    refact fill and created fill2 using map function recursive

--- src/prototypes/fill.js
+++ src/prototypes/fill.js
@@ -1,9 +1,16 @@
 const empty = require('../utils/empty')
+const map = require('./map')
 
 const fill = (array, value) => {
   return empty(array)
   ? array
-  : [array[0] === value ? value : value].concat(fill(array.slice(1), value))
+  : [value].concat(fill(array.slice(1), value))
 }
 
-module.exports = fill
+const fill2 = (array, value) => {
+  return empty(array)
+  ? []
+  : map(x => (x - x) + value, array)
+}
+
+module.exports = { fill, fill2 }
