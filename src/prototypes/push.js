c374520e6be3bf807f032fdc92adef6f969ce7fe
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-18 23:45:26 -0200

    created push function

---
+++ src/prototypes/push.js
@@ -0,0 +1,9 @@
+const empty = require('../utils/empty')
+
+const push = (array, ...value) => {
+  return empty(array)
+  ? array.concat(...value)
+  : [array[0]].concat(push(array.slice(1), ...value))
+}
+
+module.exports = push
