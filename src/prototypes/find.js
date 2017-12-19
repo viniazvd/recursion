a75d544ff145dc8a0643d932ff17f9021d2401c7
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-19 14:10:20 -0200

    refactoring to return undefined when value is not found

--- src/prototypes/find.js
+++ src/prototypes/find.js
@@ -4,7 +4,7 @@ const find = (fn, array) => {
   if (typeof fn !== 'function') throw new TypeError()
 
   return empty(array)
-    ? []
+    ? undefined
     : fn(array[0]) ? array[0] : find(fn, array.slice(1))
 }
 
