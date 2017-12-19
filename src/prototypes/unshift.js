be27d06be212f69415d6586a6ca110e53fdd8203
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-18 23:34:44 -0200

    refactoring to receving one ou more values

--- src/prototypes/unshift.js
+++ src/prototypes/unshift.js
@@ -1,9 +1,9 @@
 const empty = require('../utils/empty')
 
-const unshift = (array, value) => {
+const unshift = (array, ...value) => {
   return empty(array)
   ? []
-  : value ? [value].concat([array[0]].concat(unshift(array.slice(1)))) : [array[0]].concat(unshift(array.slice(1)))
+  : value ? [...value].concat([array[0]].concat(unshift(array.slice(1)))) : [array[0]].concat(unshift(array.slice(1)))
 }
 
 module.exports = unshift
--- src/test/unshift.test.js
+++ src/test/unshift.test.js
@@ -5,7 +5,11 @@ describe('unshift', () => {
     expect(unshift([], 1)).toEqual([])
   })
 
-  it('when is valid', () => {
+  it('when is valid with one value', () => {
     expect(unshift([2, 3, 4, 5], 1)).toEqual([1, 2, 3, 4, 5])
+  })
+
+  it('when is valid with one or more values', () => {
+    expect(unshift([2, 3, 4, 5], 0, 1)).toEqual([0, 1, 2, 3, 4, 5])
   })
 })
