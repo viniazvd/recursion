2660c7dd421bf953506ec02aaa81b579791f155a
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-20 01:24:38 -0200

    refactoring...

--- src/prototypes/lastIndexOf.js
+++ src/prototypes/lastIndexOf.js
@@ -2,7 +2,9 @@ const empty = require('../utils/empty')
 
 const lastIndexOf = (value, array, index = 0, arrayIndex = []) => {
   return empty(array)
-    ? arrayIndex.sort((x, y) => y - x)[0] || -1
+    // ? arrayIndex.sort((x, y) => y - x)[0] || -1
+    // ? arrayIndex.slice(-1)[0] || -1
+    ? arrayIndex.pop() || -1
     : array[0] === value
       ? lastIndexOf(value, array.slice(1), index + 1, arrayIndex.concat(index))
       : lastIndexOf(value, array.slice(1), index + 1, arrayIndex)
