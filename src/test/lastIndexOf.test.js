fcfb3edc3ff432c5a657886f451ee3edc7a1ac7c
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-20 01:16:45 -0200

    descoment tests (ignore this)

--- src/test/lastIndexOf.test.js
+++ src/test/lastIndexOf.test.js
@@ -1,27 +1,27 @@
-// const lastIndexOf = require('../prototypes/lastIndexOf')
+const lastIndexOf = require('../prototypes/lastIndexOf')
 
-// describe('lastIndexOf', () => {
-//   it('empty array', () => {
-//     expect(lastIndexOf(3, [])).toEqual(-1)
-//   })
+describe('lastIndexOf', () => {
+  it('empty array', () => {
+    expect(lastIndexOf(3, [])).toEqual(-1)
+  })
 
-//   it('find 2 in array', () => {
-//     expect(lastIndexOf(2, [1, 2, 3, 4, 5, 6])).toEqual(1)
-//   })
+  it('find 2 in array', () => {
+    expect(lastIndexOf(2, [1, 2, 3, 4, 5, 6])).toEqual(1)
+  })
 
-//   it('find last index found', () => {
-//     expect(lastIndexOf(2, [0, 2, 1, 2, 1, 2, 4, 2, 1])).toEqual(7)
-//   })
+  it('find last index found', () => {
+    expect(lastIndexOf(2, [0, 2, 1, 2, 1, 2, 4, 2, 1])).toEqual(7)
+  })
 
-//   it('find last index found', () => {
-//     expect(lastIndexOf(1, [0, 2, 1, 2, 1, 2, 4, 2, 1])).toEqual(8)
-//   })
+  it('find last index found', () => {
+    expect(lastIndexOf(1, [0, 2, 1, 2, 1, 2, 4, 2, 1])).toEqual(8)
+  })
 
-//   it('no founded value', () => {
-//     expect(lastIndexOf(7, [1, 2, 3, 4, 5, 6])).toEqual(-1)
-//   })
+  it('no founded value', () => {
+    expect(lastIndexOf(7, [1, 2, 3, 4, 5, 6])).toEqual(-1)
+  })
 
-//   it('find correct index when the value isn\'t the index', () => {
-//     expect(lastIndexOf(12, [10, 11, 12, 13, 14])).toEqual(2)
-//   })
-// })
+  it('find correct index when the value isn\'t the index', () => {
+    expect(lastIndexOf(12, [10, 11, 12, 13, 14])).toEqual(2)
+  })
+})
