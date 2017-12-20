b6a707e3c267d595227495d7a1b618f44eae98cf
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-19 23:25:58 -0200

    deleted old fill

--- src/test/fill.test.js
+++ src/test/fill.test.js
@@ -1,18 +1,15 @@
-const { fill, fill2 } = require('../prototypes/fill')
+const fill = require('../prototypes/fill')
 
 describe('fill', () => {
   it('array empty', () => {
     expect(fill([])).toEqual([])
-    expect(fill2([])).toEqual([])
   })
 
   it('when is valid', () => {
     expect(fill([1, 2, 3], 4)).toEqual([4, 4, 4])
-    expect(fill2([1, 2, 3], 4)).toEqual([4, 4, 4])
   })
 
   it('when the value is also inside', () => {
     expect(fill([1, 1, 1], 1)).toEqual([1, 1, 1])
-    expect(fill2([1, 1, 1], 1)).toEqual([1, 1, 1])
   })
 })
