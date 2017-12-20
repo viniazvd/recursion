dbdc3b48bbde9f0bfaa0f145662d25dcb0e7ea81
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-19 23:51:15 -0200

    deleted

--- src/utils/countRepeated.js
+++
@@ -1,7 +0,0 @@
-const len = require('../prototypes/len')
-const filter = require('../prototypes/filter')
-const isEqual = x => x === 10
-
-const countRepeated = (find, array) => len(filter(isEqual, array))
-
-module.exports = countRepeated
