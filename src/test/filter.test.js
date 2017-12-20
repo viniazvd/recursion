0cdd9523bef673e2479619b3e8f5bf30851da7e1
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-19 23:52:52 -0200

    len function removed

--- src/test/filter.test.js
+++ src/test/filter.test.js
@@ -1,15 +1,4 @@
-const len = require('../prototypes/len')
 const filter = require('../prototypes/filter')
-
-describe('len', () => {
-  it('empty array', () => {
-    expect(len([])).toEqual(0)
-  })
-
-  it('n elements', () => {
-    expect(len([1, 2, 3])).toEqual(3)
-  })
-})
 
 describe('filter', () => {
   const greaterThan2 = x => x > 2
