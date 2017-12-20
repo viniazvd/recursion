5e227310b3a18adc2866d65cee34ec32fc3e35ab
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-19 23:25:46 -0200

    delet old fill

--- src/prototypes/fill.js
+++ src/prototypes/fill.js
@@ -1,14 +1,7 @@
-const empty = require('../utils/empty')
 const map = require('./map')
 
 const fill = (array, value) => {
-  return empty(array)
-  ? array
-  : [value].concat(fill(array.slice(1), value))
-}
-
-const fill2 = (array, value) => {
   return map(x => value, array)
 }
 
-module.exports = { fill, fill2 }
+module.exports = fill
