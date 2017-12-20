7655fa44f19d7346c815be7378e378e7590d9bf7
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-20 00:45:24 -0200

    refactoring...

--- src/prototypes/pop.js
+++ src/prototypes/pop.js
@@ -1,9 +1,9 @@
 const empty = require('../utils/empty')
 
 const pop = (array) => {
-  return empty(array)
+  return empty(array) || array.length <= 1
   ? []
-  : array.length > 1 ? [array[0]].concat(pop(array.slice(1))) : []
+  : [array[0]].concat(pop(array.slice(1)))
 }
 
 module.exports = pop
